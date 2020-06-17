936dbe6cfdd0e39d8ef0d826dfe6ba4a1c3ca1c8
Adlan <siribeam@gmail.com>
2020-06-17 21:43:46 +0700

    update daftar init

--- application/views/Daftar.php
+++ application/views/Daftar.php
@@ -12,24 +12,44 @@
           <div class="card-body">
             
             <form action="<?php base_url('Daftar/input') ?>" method="post">
-              <!-- Name -->  
+              <!-- Kode Sekolah -->  
               <div class="form-group">
-                <input type="text" class="form-control" id="nama" name="nama" placeholder="Name">
-                <small class="form-text text-danger"><?= form_error('nama') ?></small>
+                <input type="text" class="form-control" id="kode_sekolah" name="Kode_Sekolah" placeholder="Kode Sekolah">
+                <small class="form-text text-danger"><?= form_error('kode_sekolah') ?></small>
+              </div>
+              <!-- Nama Sekolah -->  
+              <div class="form-group">
+                <input type="text" class="form-control" id="nama_sekolah" name="Nama_Sekolah" placeholder="Nama Sekolah">
+                <small class="form-text text-danger"><?= form_error('nama_sekolah') ?></small>
               </div>
               <!-- Email -->
               <div class="form-group">
-                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                <small class="form-text text-danger"><?= form_error('email') ?></small>
+                <input type="email" class="form-control" id="email_sekolah" name="Email_Sekolah" placeholder="Email Sekolah">
+                <small class="form-text text-danger"><?= form_error('email_sekolah') ?></small>
               </div>
               <!-- Username -->
               <div class="form-group">
-                <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-                <small class="form-text text-danger"><?= form_error('username') ?></small>
+                <input type="text" class="form-control" id="alamat_sekolah" name="Alamat_Sekolah" placeholder="Alamat Sekolah">
+                <small class="form-text text-danger"><?= form_error('alamat_sekolah') ?></small>
               </div>
               <!-- Password -->
               <div class="form-group">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                <input type="number" class="form-control" id="notelp_sekolah" name="notelp_sekolah" placeholder="No. Telp Sekolah">
+                <small class="form-text text-danger"><?= form_error('notelp+sekolah') ?></small>
+              </div>
+              <!-- Password -->
+              <div class="form-group">
+                <input type="email" class="form-control" id="email_user" name="email_user" placeholder="Email Pengguna">
+                <small class="form-text text-danger"><?= form_error('password') ?></small>
+              </div>
+              <!-- Password -->
+              <div class="form-group">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Kata Sandi">
+                <small class="form-text text-danger"><?= form_error('password') ?></small>
+              </div>
+              <!-- Password -->
+              <div class="form-group">
+                <input type="password" class="form-control" id="re_password" name="re_password" placeholder="Ulangi Kata Sandi">
                 <small class="form-text text-danger"><?= form_error('password') ?></small>
               </div>
               <center>
