56c119dd1420d8334136beccb5884d8a9f473832
Faizal Akbari Putra <49709338+faizalakbari@users.noreply.github.com>
2020-06-24 01:15:09 +0700

    add login model

---
+++ application/models/Login_model.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Login_model extends CI_Model {
+    function checklogin($email_user,$password) {
+        $this->db->where('email_user', $email_user);
+        $this->db->where('password', $password);
+        $query =  $this->db->get('user');
+        return $query->num_rows();
+    }
+    function data_login($email_user,$password) {
+        $this->db->where('email_user', $email_user);
+        $this->db->where('password', $password);
+        return $this->db->get('user')->row();
+    }
+
+
+}
+
