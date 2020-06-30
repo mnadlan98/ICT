bdcf6f43bad57cf217f77f4176512c815d91dc32
SynisterWallace <hectorclarkson@gmail.com>
2020-06-30 17:21:08 +0700

    fix

--- application/views/home/registrasi.php
+++ application/views/home/registrasi.php
@@ -14,7 +14,7 @@
   <h5 style="font-weight: bolder; color: black; padding-left: 9em; margin-top: 5px; "><strong style="border-bottom: 3px solid red; font-size:24px; ">Daftar</strong></h5></br></br>
       <?php echo form_open('Register/index');?>
           <div class="form-group">
-            <select class="form-control" style="padding-left:3em;" id="jenjang_sekolah" name="jenjang_sekolah">
+            <select class="form-control"   id="jenjang_sekolah" name="jenjang_sekolah">
               <option selected disabled hidden>Jenjang Sekolah</option>
               <option value="SLB">SLB</option>
               <option value="SD">SD</option>
@@ -25,47 +25,45 @@
             <small class="form-text text-danger"><?php echo form_error('jenjang_sekolah'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="kota_sekolah" name="kota_sekolah" placeholder="Kota/Kabupaten" value="<?php echo set_value('kota_sekolah'); ?>" style="padding-left:3em;">
-            <i class="fa fa-id-card icon  " style="position: absolute; margin-bottom: 10px; top:118px; left:28px; "></i>
+            <input type="text" class="form-control" id="kota_sekolah" name="kota_sekolah" placeholder="Kota/Kabupaten" value="<?php echo set_value('kota_sekolah'); ?>"  >
             <small class="form-text text-danger"><?php echo form_error('kota_sekolah'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="nama_sekolah" name="nama_sekolah" placeholder="Nama Sekolah" value="<?php echo set_value('nama_sekolah'); ?>" style="padding-left:3em;">
+            <input type="text" class="form-control" id="nama_sekolah" name="nama_sekolah" placeholder="Nama Sekolah" value="<?php echo set_value('nama_sekolah'); ?>"  >
             <small class="form-text text-danger"><?php echo form_error('nama_sekolah'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="email_sekolah" name="email_sekolah"  placeholder="Email Sekolah" value="<?php echo set_value('email_sekolah'); ?>" style="padding-left:3em;">
-            <i class="fa fa-envelope icon  " style="position: absolute; margin-bottom: 10px; top:225.5px; left:29px; "></i>
+            <input type="text" class="form-control" id="email_sekolah" name="email_sekolah"  placeholder="Email Sekolah" value="<?php echo set_value('email_sekolah'); ?>"  >
             <small class="form-text text-danger"><?php echo form_error('email_sekolah'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="notelp_sekolah" name="notelp_sekolah" placeholder="Nomor Telepon Sekolah" value="<?php echo set_value('notelp_sekolah'); ?>" style="padding-left:3em;">
-            <i class="fa fa-phone icon  " style="position: absolute; margin-bottom: 10px; top:332px; left:28px; "></i>
+            <input type="text" class="form-control" id="notelp_sekolah" name="notelp_sekolah" placeholder="Nomor Telepon Sekolah" value="<?php echo set_value('notelp_sekolah'); ?>"  >
+
             <small class="form-text text-danger"><?php echo form_error('notelp_sekolah'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="nama_user" name="nama_user" placeholder="Nama Pengguna" value="<?php echo set_value('nama_user'); ?>" style="padding-left:3em;">
-            <i class="fa fa-id-card icon  " style="position: absolute; margin-bottom: 10px; top:118px; left:28px; "></i>
+            <input type="text" class="form-control" id="nama_user" name="nama_user" placeholder="Nama Pengguna" value="<?php echo set_value('nama_user'); ?>" >
+
             <small class="form-text text-danger"><?php echo form_error('nama_user'); ?></small>
           </div>
           <div class="form-group">
-            <input type="email" class="form-control" id="email_user" name="email_user" placeholder="Email Pengguna" value="<?php echo set_value('email_user'); ?>" style="padding-left:3em;">
-            <i class="fa fa-envelope icon  " style="position: absolute; margin-bottom: 10px; top:386px; left:28px; "></i>
+            <input type="email" class="form-control" id="email_user" name="email_user" placeholder="Email Pengguna" value="<?php echo set_value('email_user'); ?>"  >
+
             <small class="form-text text-danger"><?php echo form_error('email_user'); ?></small>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" id="notelp_user" name="notelp_user" placeholder="Nomor Telepon Pengguna" value="<?php echo set_value('notelp_user'); ?>" style="padding-left:3em;">
-            <i class="fa fa-phone icon  " style="position: absolute; margin-bottom: 10px; top:332px; left:28px; "></i>
+            <input type="text" class="form-control" id="notelp_user" name="notelp_user" placeholder="Nomor Telepon Pengguna" value="<?php echo set_value('notelp_user'); ?>"  >
+
             <small class="form-text text-danger"><?php echo form_error('notelp_user'); ?></small>
           </div>
           <div class="form-group">
-            <input type="password" class="form-control" id="password" name="password" placeholder="Kata Sandi" value="<?php echo set_value('password'); ?>" style="padding-left:3em;">
-            <i class="fa fa-key icon  " style="position: absolute; margin-bottom: 10px; top:440px; left:28px; "></i>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Kata Sandi" value="<?php echo set_value('password'); ?>"  >
+
             <small class="form-text text-danger"><?php echo form_error('password'); ?></small>
           </div>
           <div class="form-group">
-            <input type="password" class="form-control" id="password" name="password_conf" placeholder="Ulangi Kata Sandi" value="<?php echo set_value('password_conf'); ?>" style="padding-left:3em;">
-            <i class="fa fa-key icon  " style="position: absolute; margin-bottom: 10px; top:494px; left:28px; "></i>
+            <input type="password" class="form-control" id="password" name="password_conf" placeholder="Ulangi Kata Sandi" value="<?php echo set_value('password_conf'); ?>"  >
+
             <small class="form-text text-danger"><?php echo form_error('password_conf'); ?></small>
           </div>
           <div class="form-check">
