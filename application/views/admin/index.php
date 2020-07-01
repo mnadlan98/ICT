fd24e18609a67448712ad1b795a01cccc0728880
Faizal Akbari Putra <49709338+faizalakbari@users.noreply.github.com>
2020-07-01 09:21:51 +0700

    index page admin

---
+++ application/views/admin/index.php
@@ -0,0 +1,46 @@
+    <div class="container-fluid">
+      <div class="row">
+          <aside>
+              <div class="col-sm-3 col-md-2 sidebar">
+                <ul class="nav nav-sidebar">
+                  <li ><a href="#"><i class="fa fa-home fa-lg"></i> Home</a></li>
+                  <li><a href="#"><i class="fa fa-archive fa-lg"></i> Pengajuan</a></li>
+                  <li><a href="#"><i class="fa fa-th-list fa-lg"></i> Konten Web</a></li>
+                  <li><a href="#"><i class="fa fa-users fa-lg"></i> Users</a></li>
+                </ul>
+              </div>
+          </aside>
+          <section id="konten">
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+              <h1 class="page-header">Home Admin</h1>
+              <div class="row placeholders">
+                <div class="col-xs-6 col-sm-3 placeholder">
+                  <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                  <h4>Label</h4>
+                  <span class="text-muted">Something else</span>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                  <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                  <h4>Label</h4>
+                  <span class="text-muted">Something else</span>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                  <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                  <h4>Label</h4>
+                  <span class="text-muted">Something else</span>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                  <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                  <h4>Label</h4>
+                  <span class="text-muted">Something else</span>
+                </div>
+              </div>
+            </div>
+          </section>
+      </div>
+    </div>
+    <script src="../bootstrap/js/jquery.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../bootstrap/js/holder.js"></script>
+  </body>
+</html>
