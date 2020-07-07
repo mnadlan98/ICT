8b9e5419de78476edec4bf7662f7c89c1d983046
SynisterWallace <hectorclarkson@gmail.com>
2020-07-07 19:29:52 +0700

    fixes

--- application/views/home/adminlogin.php
+++ application/views/home/adminlogin.php
@@ -6,7 +6,7 @@
   <img src="../images/logo.jpg" style="width:280px; height:140px; position: absolute; left:720px; top: 40px;">
   </div>
   <div class="col-md-4 py-3" style=" background-color: #222222;">
-  <h5 style="font-weight: bolder; color: white; padding-left: 9em; margin-top: 8em; "><strong style="border-bottom: 3px solid red; font-size:24px; ">Masuk</strong></h5></br></br>
+  <h5 style="font-weight: bolder; color: white; padding-left: 11em; margin-top: 8em; "><strong style="border-bottom: 3px solid red; font-size:24px; ">Masuk</strong></h5></br></br>
      <?php // form_open(base_url('MainController/login')); ?> 
     <form method="post">
       <div class="form-group">
@@ -22,18 +22,18 @@
         <a href="#" onclick="forgetpass()" style="color: light-blue; text-decoration: none; padding-left:1vh; font-family: Lato;"><small><u>Lupa Password?</u></small></a>
       </div>
       <div class="row">
-        <div class="col-md-12" style="padding-left:12em;">
+        <div class="col-md-12" style="padding-left:15em;">
           <button type="submit" name="login" class="btn btn-outline-dark" style="background: #BD0306; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 7px;font-weight: bold; font-size: 20px; font-family: Lato; color:white; ">Masuk</button>              
         </div>
         
       </div>
-      <div class="form-group" style="margin-bottom:14em;">
-      <p style="font-size:13px; padding-left:12em; font-family: Lato; padding-top:10px; color:white;">Registrasi Admin <a href="<?php echo base_url().'MainController/viewRegistrasi';?>" style="color: light-blue; text-decoration: none; font-size:15px; font-family: Lato;"><small><u>Disni</u></small></a></p>
+      <div class="form-group" style="margin-bottom:15em;">
+      <p style="font-size:13px; padding-left:16em; font-family: Lato; padding-top:10px; color:white;">Registrasi Admin <a href="<?php echo base_url().'MainController/viewRegistrasi';?>" style="color: light-blue; text-decoration: none; font-size:15px; font-family: Lato;"><small><u>Disni</u></small></a></p>
         
       </div>
       <div class="row">
           
-          <div class="col" style=" margin-left:24em;">
+          <div class="col" style=" margin-left:28em;">
           <a class="btn btn-outline-dark" href="<?php echo base_url().'MainController/viewLogin';?>" style="background: #BD0306; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 7px;font-weight: bold; font-size: 20px; font-family: Lato; color:white; ">User</a>            
           </div>
       </div>
--- application/views/home/login.php
+++ application/views/home/login.php
@@ -9,7 +9,7 @@
   <img src="../images/logo.jpg" style="width:280px; height:140px; position: absolute; left:720px; top: 40px;">
   </div>
   <div class="col-md-4 py-3" style=" background-color: #D7D7D7;" >
-  <h5 style="font-weight: bolder; color: black; padding-left: 9em; margin-top: 8em; "><strong style="border-bottom: 3px solid red; font-size:24px; ">Masuk</strong></h5></br></br>
+  <h5 style="font-weight: bolder; color: black; padding-left: 11em; margin-top: 8em; "><strong style="border-bottom: 3px solid red; font-size:24px; ">Masuk</strong></h5></br></br>
     <form action="<?php base_url('login/inputlogin') ?>" method="post">
       <div class="form-group">
         
@@ -26,16 +26,16 @@
         <a href="#" onclick="forgetpass()" style="color: light-blue; text-decoration: none; padding-left:1vh; font-family: Lato;" ><small><u>Lupa Password?</u></small></a>
       </div>
       <div class="row">
-        <div class="col-md-12" style="padding-left:12em;">
+        <div class="col-md-12" style="padding-left:15em;">
           <button type="submit" name="login" class="btn btn-outline-dark" style="background: #BD0306; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 7px;font-weight: bold; font-size: 20px; font-family: Lato; color:white; ">Masuk</button>              
         </div>
         
       </div>
-      <div class="form-group" style="margin-bottom:10em;">
-      <p style="font-size:13px; padding-left:10.5em; font-family: Lato; padding-top:10px;">Belum punya akun? <a href="<?php echo base_url().'MainController/viewRegistrasi';?>" style="color: light-blue; text-decoration: none; font-size:15px; font-family: Lato;"><small><u>Daftar Disni</u></small></a></p>
+      <div class="form-group" style="margin-bottom:14em;">
+      <p style="font-size:13px; padding-left:13.5em; font-family: Lato; padding-top:10px;">Belum punya akun? <a href="<?php echo base_url().'MainController/viewRegistrasi';?>" style="color: light-blue; text-decoration: none; font-size:15px; font-family: Lato;"><small><u>Daftar Disni</u></small></a></p>
         
       </div>
-      <div class="col-md-12" style="padding-left:24em;">
+      <div class="col-md-12" style="padding-left:27em;">
           <a class="btn btn-outline-dark" href="<?php echo base_url().'MainController/viewAdminlogin';?>" style="background: #BD0306; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 7px;font-weight: bold; font-size: 20px; font-family: Lato; color:white; ">Admin</a>              
         </div>
 
