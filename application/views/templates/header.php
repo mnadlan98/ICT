5c9acafcb0619c93a11d20df5c8a2202e16a9e64
Faizal Akbari Putra <49709338+faizalakbari@users.noreply.github.com>
2020-07-14 19:03:31 +0700

    update js bootstrap 4.5.0

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,8 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
 
   <!-- Footer -->
@@ -34,7 +36,7 @@
     <a class="item" href="#about">Kontak Kami</a>
     <?php if ($this->session->userdata("user")['logged']): ?>
       <a class="nav-link" href="<?php echo site_url()."MainController/viewProfil"?>" style="padding-left:10px; border-radius:0px; border-left:3px solid red;">Profil</a>
-      <a class="item" href="<?php echo site_url()."MainController/index#review"?>">Review Pengajuan</a>
+      <a class="item" href="<?php echo site_url()."MainController/review"?>">Review Pengajuan</a>
     <a class="item" href="<?php echo site_url()."MainController/logout"?>" style="background-color:#DCDCDC;" onmouseover="this.style.color='red'" onmouseout="this.style.color='black'">Logout</a>
     <?php endif ?>
   </div>
